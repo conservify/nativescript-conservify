8401781c25c848cfd6e502e9c04f02e00687b413
Jacob Lewallen <jlewallen@gmail.com>
2019-10-25 15:31:26 -0700

    Stubs for iOS.

--- src/conservify.ios.ts
+++ src/conservify.ios.ts
@@ -1,5 +1,40 @@
 import { Common } from './conservify.common';
 
 export class Conservify extends Common {
+    constructor() {
+        super();
+        this.active = {};
+    }
 
+    public start(serviceType) {
+        console.log("initialize");
+
+        const active = this.active;
+
+        return Promise.resolve({ });
+    }
+
+    public json(info) {
+        return new Promise((resolve, reject) => {
+            reject();
+        });
+    }
+
+    public protobuf(info) {
+        return new Promise((resolve, reject) => {
+            reject();
+        });
+    }
+
+    public download(info) {
+        return new Promise((resolve, reject) => {
+            reject();
+        });
+    }
+
+    public scanNetworks() {
+        return new Promise((resolve, reject) => {
+            reject();
+        });
+    }
 }
